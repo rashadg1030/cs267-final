42574e47f24add128efdc5a4c8b913a715842b16
rashadg1030 <rashad.sasaki@gmail.com>
2021-05-13 15:35:47 -0700

    Add code for particle simulation

--- src/particle/AccParticle.hs
+++ src/particle/AccParticle.hs
@@ -1,6 +1,6 @@
 module AccParticle where
 
-import Data.Array.Accelerate                    as A
+import Data.Array.Accelerate                    as A hiding (V2)
 import Data.Array.Accelerate.Control.Lens
 import Data.Array.Accelerate.Linear.V2
 import Data.Array.Accelerate.Linear.Metric
@@ -34,8 +34,8 @@ advanceParticles
   -> Acc (Vector Particle)
 advanceParticles calcAccels timeStep ps =
   let
-    accels = calcAccels $ A.map pointMassOfBody ps
-    advance b a = advanceBody (the timeStep) (setAccelOfBody a b)
+    accels = calcAccels $ A.map particlePosMass ps
+    advance b a = moveParticle (the timeStep) (setParticleAccel a b)
   in
     A.zipWith advance ps accels
 
@@ -46,13 +46,10 @@ advanceWorld
     -> World
 advanceWorld advance timeStep world =
   let
-    -- Update the bodies
     particles' = advance (fromList Z [timeStep]) (worldParticles world)
 
-    -- Update the world
     steps'  = worldSteps world + 1
     time'   = worldTime  world + timeStep
-
   in
     world { worldParticles = particles'
           , worldSteps = steps'
@@ -62,26 +59,65 @@ advanceWorld advance timeStep world =
 putParticle :: Exp Pos -> Exp Particle
 putParticle pos = body
   where
-    pointmass = lift (pos, constant 1)                    :: Exp PointMass
-    body      = lift (pointmass, constant 0, constant 0)  :: Exp Body
+    posmass = lift (pos, constant 1) :: Exp PosMass
+    body = lift (posmass, constant 0, constant 0)  :: Exp Particle
 
 particleVel :: Exp Particle -> Exp Vel
 particleVel = view _2
 
-
--- | Take the Acceleration of a Body
---
 particleAccel :: Exp Particle -> Exp Accel
 particleAccel = view _3
 
-
--- | Take the PointMass of a Body
---
 particlePosMass :: Exp Particle -> Exp PosMass
 particlePosMass = view _1
 
 posMassPos :: Exp PosMass -> Exp Pos
 posMassPos = A.fst
 
-massPosMass :: Exp PointMass -> Exp Mass
-massPosMass = A.snd
+posMassMass :: Exp PosMass -> Exp Mass
+posMassMass = A.snd
+
+
+setParticleMass :: Exp Mass -> Exp Particle -> Exp Particle
+setParticleMass mass p = lift (posMass, vel, acc)
+  where
+    vel = particleVel p
+    acc = particleAccel p
+    pos = posMassPos (particlePosMass p)
+    posMass = lift (pos, mass) :: Exp PosMass
+
+
+setParticleAccel :: Exp Accel -> Exp Particle -> Exp Particle
+setParticleAccel acc p = lift (pm, vel, acc)
+  where
+    pm          = particlePosMass p
+    vel         = particleVel p
+
+
+setParticleStartVel :: Exp Float -> Exp Particle -> Exp Particle
+setParticleStartVel startVel p = lift (pm, vel'', acc)
+  where
+    pm          = particlePosMass p
+    acc         = particleAccel p
+    pos         = posMassPos pm
+
+    pos'        = normalize pos
+    vel'        = lift (V2 y' (-x'))
+
+    vel''       = (sqrt (norm pos) * startVel) *^ vel'
+
+    V2 x' y' = unlift pos' :: V2 (Exp Float)
+
+
+moveParticle :: Exp Time -> Exp Particle -> Exp Particle
+moveParticle time p = lift ( pm', vel', acc )
+  where
+    pm          = particlePosMass p
+    pos         = posMassPos pm
+    vel         = particleVel p
+    acc         = particleAccel p
+    mass        = posMassMass pm
+
+    pm'         = lift (pos', mass) :: Exp PosMass
+    pos'        = pos + time *^ vel
+    vel'        = vel + time *^ acc
