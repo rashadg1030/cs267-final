46acc73902641fdf2e983837083bde99663a4384
rashadg1030 <rashad.sasaki@gmail.com>
2021-05-13 09:55:35 -0700

    Add matMul function for REPA

--- src/matmul/RepaMatMul.hs
+++ src/matmul/RepaMatMul.hs
@@ -1,3 +1,17 @@
 module RepaMatMul where
 
+import Data.Array.Repa (Array(..), U(..), Z(..), DIM2(..), (:.)(..))
+import Data.Array.Repa as Repa
 
+matMul
+  :: (Monad m, Num a)
+  => Array U DIM2 a
+  -> Array U DIM2 a
+  -> m (Array U DIM2 a)
+matMul a b = sumP (Repa.zipWith (*) aRepl bRepl)
+    where
+      t     = Repa.transpose b
+      aRepl = extend (Z :.All :.colsB :.All) a
+      bRepl = extend (Z :.rowsA :.All :.All) t
+      (Z :.colsA :.rowsA) = extent a
+      (Z :.colsB :.rowsB) = extent b
